7c4af5d422fdb33bf08ff3814ae4ad19d96c9631
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-17 13:27:24 +0100

    remove comment

--- src/state/actions/actions.js
+++ src/state/actions/actions.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
 export const thunkExperiment = () => {
-  //   return (dispatch) => {
-  //     axios.get("https://reqres.in/api/users").then((response) => {
-  //       dispatch({
-  //         type: "SET_USERS",
-  //         payload: response.data.data,
-  //       });
-  //     });
   return async (dispatch) => {
     const response = await axios.get("https://reqres.in/api/users");
 
